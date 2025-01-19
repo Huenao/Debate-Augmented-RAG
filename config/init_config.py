00526fcea9ed49ee9f0b81eea8ae020a0bfa2bd6
Wentao Hu <hwt0309@gmail.com>
2025-01-19 15:32:20 +0800

    update

--- config/init_config.py
+++ config/init_config.py
@@ -8,7 +8,8 @@ def init_cfg():
     # Create argument parser
     parser = argparse.ArgumentParser("Update configuration from command line arguments.")
 
-    parser.add_argument("--method_name", type=str, help="Name of the method")
+    parser.add_argument("--method_name", type=str, help="Name of the method",
+                        default="naive_rag")
     parser.add_argument("--config_path", type=str, help="Path to the config.json file")
     
     # Global Paths
@@ -39,7 +40,7 @@ def init_cfg():
     if args.config_path is None:
     # warn if config file not found
         print("Warning: No config file provided. Using base configuration.")
-        config = Config("./config/base_config.yaml")
+        config = Config("./config/base_config.yaml", {"save_note": args.method_name})
         
     else:
         if not os.path.exists(args.config_path):
@@ -52,7 +53,7 @@ def init_cfg():
         for key, value in vars(args).items():
             if key != "config_path" and value is not None:  # Skip config_path and only update provided arguments
                 config[key] = value
-            
+        config["save_note"] = args.method_name
         config =  Config("./config/base_config.yaml", config)
     
     return config
